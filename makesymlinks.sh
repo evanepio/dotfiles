6d38e453680f5b697c3002e244d61f62dec33c02
evanepio <evan.porter@dotman.ca>
2012-08-05 01:56:19 -0300

    Adding plugins, nerdtree as first plugin.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,7 +11,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc tmux.conf"    # list of files/folders to symlink in homedir
+files="bashrc vimrc tmux.conf vim"    # list of files/folders to symlink in homedir
 
 ##########
 
