a5c0ec26dcb8b8ec4210d1a82044ee362b792220
evanepio <evan.porter@dotman.ca>
2012-08-05 14:50:12 -0300

    Adding global gitignore file

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,7 +11,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc tmux.conf vim"    # list of files/folders to symlink in homedir
+files="bashrc vimrc tmux.conf vim gitignore_global"    # list of files/folders to symlink in homedir
 
 ##########
 
